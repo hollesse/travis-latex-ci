62070315e70f57e3e6a7630a2881a012415bfa24
Joshua Töpfer <privat@joshuatoepfer.de>
2017-01-27 08:57:51 +0100

    try to fix it

--- ci/replaceumlaute.sh
+++ ci/replaceumlaute.sh
@@ -6,15 +6,33 @@
 # required
 
 
-replaceUmlaute() {
-    sed -i 's;ä;{\\"a};g' $2.$1 
-    sed -i 's;Ä;{\\"A};g' $2.$1
-    sed -i 's;ö;{\\"o};g' $2.$1
-    sed -i 's;Ö;{\\"O};g' $2.$1
-    sed -i 's;ü;{\\"u};g' $2.$1
-    sed -i 's;Ü;{\\"U};g' $2.$1
-    sed -i 's;ß;{\\ss};g' $2.$1
-}
+#replaceUmlaute() {
+#    sed -i 's;ä;{\\"a};g' $2.$1 
+#    sed -i 's;Ä;{\\"A};g' $2.$1
+#    sed -i 's;ö;{\\"o};g' $2.$1
+#    sed -i 's;Ö;{\\"O};g' $2.$1
+#    sed -i 's;ü;{\\"u};g' $2.$1
+#    sed -i 's;Ü;{\\"U};g' $2.$1
+#    sed -i 's;ß;{\\ss};g' $2.$1
+#}
+
+#replaceUmlaute tex \*
+#replaceUmlaute tex template\/
+#replaceUmlaute tex userdata\/
+#replaceUmlaute tex userdata\/lib\/listing\/
+#replaceUmlaute tex userdata\/ExampleContent\/content\/
+#replaceUmlaute tex userdata\/ExampleContent\/content\/chapters\/
+#replaceUmlaute bib userdata\/ExampleContent\/content\/resources\/
+
+
+sed -i 's;ä;{\\"a};g' *.tex 
+sed -i 's;Ä;{\\"A};g' *.tex
+sed -i 's;ö;{\\"o};g' *.tex
+sed -i 's;Ö;{\\"O};g' *.tex
+sed -i 's;ü;{\\"u};g' *.tex
+sed -i 's;Ü;{\\"U};g' *.tex
+sed -i 's;ß;{\\ss};g' *.tex
+
 
 replaceUmlaute tex \*
 replaceUmlaute tex template\/
