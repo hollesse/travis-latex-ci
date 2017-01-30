8c20136b269f95b0b53fa478eabb5ff7daf266eb
Joshua Töpfer <privat@joshuatoepfer.de>
2017-01-30 16:10:26 +0100

    SED

--- ci/replaceumlaute.sh
+++ ci/replaceumlaute.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 replaceUmlaute(){
-  sed -i -- 's;Ã¶;oe;g' $1*.$2 # ö
-  sed -i -- 's;Ã–;Oe;g' $1*.$2 # Ö
+  sed -i -- 's;ö;oe;g' $1*.$2 # ö
+  sed -i -- 's;Ö;Oe;g' $1*.$2 # Ö
   sed -i -- 's;Ã¤;ae;g' $1*.$2 # ä
   sed -i -- 's;Ã„;Ae;g' $1*.$2 # Ä
   sed -i -- 's;Ã¼;ue;g' $1*.$2 # ü
