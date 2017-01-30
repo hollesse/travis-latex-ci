ec18b3848a33aa8ec8f45fc5ce0be7351ac3ca6f
Joshua Töpfer <privat@joshuatoepfer.de>
2017-01-30 14:23:23 +0100

    SED added ß

--- ci/replaceumlaute.sh
+++ ci/replaceumlaute.sh
@@ -6,4 +6,5 @@ sed -i -- 's;Ã¤;ae;g' userdata/*.tex # ä
 sed -i -- 's;Ã„;Ae;g' userdata/*.tex # Ä
 sed -i -- 's;Ã¼;ue;g' userdata/*.tex # ü
 sed -i -- 's;Ãœ;Ue;g' userdata/*.tex # Ü
+sed -i -- 's;ÃŸ;ss;g' userdata/*.tex # ß
 
