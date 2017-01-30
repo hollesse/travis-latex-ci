fe80fda925ecdc0a07b15d9e20490c94f8b444e2
Joshua Töpfer <privat@joshuatoepfer.de>
2017-01-30 14:09:14 +0100

    SED skript um andere umlaute erweitert

--- ci/replaceumlaute.sh
+++ ci/replaceumlaute.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-sed -i -- 's;Ã¶;oe;g' userdata/*.tex
+sed -i -- 's;Ã¶;oe;g' userdata/*.tex # ö
+sed -i -- 's;Ã–;Oe;g' userdata/*.tex # Ö
+sed -i -- 's;Ã¤;ae;g' userdata/*.tex # ä
+sed -i -- 's;Ã„;Ae;g' userdata/*.tex # Ä
+sed -i -- 's;Ã¼;ue;g' userdata/*.tex # ü
+sed -i -- 's;Ãœ;Ue;g' userdata/*.tex # Ü
+
