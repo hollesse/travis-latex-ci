ac171153ad5b3ff0e7450c235beb1800baaaadb2
Joshua Töpfer <privat@joshuatoepfer.de>
2017-01-30 14:28:25 +0100

    SED add function

--- ci/replaceumlaute.sh
+++ ci/replaceumlaute.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env sh
 
-sed -i -- 's;Ã¶;oe;g' userdata/*.tex # ö
-sed -i -- 's;Ã–;Oe;g' userdata/*.tex # Ö
-sed -i -- 's;Ã¤;ae;g' userdata/*.tex # ä
-sed -i -- 's;Ã„;Ae;g' userdata/*.tex # Ä
-sed -i -- 's;Ã¼;ue;g' userdata/*.tex # ü
-sed -i -- 's;Ãœ;Ue;g' userdata/*.tex # Ü
-sed -i -- 's;ÃŸ;ss;g' userdata/*.tex # ß
+replaceUmlaute(){
+  sed -i -- 's;Ã¶;oe;g' $1*.$2 # ö
+  sed -i -- 's;Ã–;Oe;g' $1*.$2 # Ö
+  sed -i -- 's;Ã¤;ae;g' $1*.$2 # ä
+  sed -i -- 's;Ã„;Ae;g' $1*.$2 # Ä
+  sed -i -- 's;Ã¼;ue;g' $1*.$2 # ü
+  sed -i -- 's;Ãœ;Ue;g' $1*.$2 # Ü
+  sed -i -- 's;ÃŸ;ss;g' $1*.$2 # ß
+}
 
+
+replaceUmlauteTex() {
+    replaceUmlaute $1 tex
+}
+
+replaceUmlauteBib() {
+    replaceUmlaute $1 bib
+}
+
+replaceUmlauteTex /userdata/
