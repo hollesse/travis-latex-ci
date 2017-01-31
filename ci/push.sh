54c1250bfd2516024877b1e03a02454834a6308f
Joshua Töpfer <privat@joshuatoepfer.de>
2017-01-31 14:44:08 +0100

    fix fix fix push.sh

--- ci/push.sh
+++ ci/push.sh
@@ -11,8 +11,8 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/hollesse/travis-latex-ci.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin-master https://${GH_TOKEN}@github.com/hollesse/travis-latex-ci.git > /dev/null 2>&1
+  git push # --quiet --set-upstream origin-master gh-master 
 }
 
 setup_git
